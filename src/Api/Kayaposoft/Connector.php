b6c495068024ace83287135c8e4a87b3e6530a16
Mantas Tarutis <mantas.tarutis@gmail.com>
2020-02-16 21:56:12 +0200

    show holidays

--- src/Api/Kayaposoft/Connector.php
+++ src/Api/Kayaposoft/Connector.php
@@ -1,10 +1,127 @@
 <?php
-
+declare(strict_types=1);
 
 namespace App\Api\Kayaposoft;
 
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Psr\Http\Message\ResponseInterface;
+
 class Connector
 {
+    private $supportedCountries;
 
+    //Api url endings
+    const PUBLIC_HOLIDAYS_BY_YEAR_AND_COUNTRY =
+        "/?action=getHolidaysForYear&year=%s&country=%s&holidayType=public_holiday";
+    const PUBLIC_HOLIDAYS_BY_YEAR_AND_COUNTRY_AND_REGION =
+        "/?action=getHolidaysForYear&year=%s&country=%s&holidayType=public_holiday&region=%s";
+    const IS_PUBLIC_HOLIDAY = "/?action=isWorkDay&date=%s&country=%s";
+    const IS_WORKDAY = "/?action=isWorkDay&date=%s&country=%s";
+
+    public function __construct()
+    {
+        $this->supportedCountries = $this->fetchSupportedCountries();
+    }
+
+    public function getHolidaysByYearAndCountry(string $year, string $countryCode) : array
+    {
+        $client = new Client;
+
+        $url = sprintf($_ENV["KAYAPOSOFT_URL"] . self::PUBLIC_HOLIDAYS_BY_YEAR_AND_COUNTRY, $year, $countryCode);
+
+        $request = $client->request("GET", $url);
+        $response = $request->getBody()->getContents();
+
+        return json_decode($response, true);
+    }
+
+    public function getHolidaysByYearAndCountryAndRegion(string $year, string $countryCode, string $region) : array
+    {
+        $client = new Client;
+
+        $url = sprintf($_ENV["KAYAPOSOFT_URL"] .
+            self::PUBLIC_HOLIDAYS_BY_YEAR_AND_COUNTRY_AND_REGION, $year, $countryCode, $region);
+
+        $request = $client->request("GET", $url);
+        $response = $request->getBody()->getContents();
+
+        return json_decode($response, true);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSupportedCountries() :array
+    {
+        return $this->supportedCountries;
+    }
+
+
+    public function fetchSupportedCountries() : array
+    {
+        $client = new Client;
+
+        $request = $client->request("GET", $_ENV["SUPPORTED_COUNTRIES_URL"]);
+        $response = $request->getBody()->getContents();
+
+        return json_decode($response, true);
+    }
+
+    public function isPublicHoliday(string $countryCode) : array
+    {
+        $client = new Client;
+
+        $url = sprintf($_ENV["KAYAPOSOFT_URL"] .
+            self::IS_PUBLIC_HOLIDAY, date("d-m-Y"), $countryCode);
+
+        $request = $client->request("GET", $url);
+        $response = $request->getBody()->getContents();
+
+        return json_decode($response, true);
+    }
+
+    public function isWorkday(string $countryCode) : array
+    {
+        $client = new Client;
+
+        $url = sprintf($_ENV["KAYAPOSOFT_URL"] .
+            self::IS_WORKDAY, date("d-m-Y"), $countryCode);
+
+        $request = $client->request("GET", $url);
+        $response = $request->getBody()->getContents();
+
+        return json_decode($response, true);
+    }
+
+    public function today(string $countryCode) : string
+    {
+        $today = "Workday";
+        $isPublicHoliday = $this->isPublicHoliday($countryCode);
+        $isWorkday = $this->isWorkday($countryCode);
+
+        if (!$isPublicHoliday && !$isWorkday) {
+            $today = "Free day";
+        }
+        if ($isPublicHoliday) {
+            $today = "Holiday";
+        }
+
+        return $today;
+    }
+
+    public function groupByMonth(array $holidaysByYear) : array
+    {
+        $grouped = [];
+
+        foreach ($holidaysByYear as $holiday) {
+            $grouped[$holiday['date']['month']][] = [
+                'date' => $holiday['date'],
+                'name' => $holiday['name']
+            ];
+        }
+//        dd($grouped);
+        return $grouped;
+    }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,64 @@
 
 namespace App\Controller;
 
+use App\Api\Kayaposoft\Connector;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/home", name="home")
-     */
-    public function index()
+
+    public function index(Connector $connector)
     {
+        $countries = $connector->getSupportedCountries();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'countries' => $countries,
         ]);
+    }
+
+    public function showHolidays(Request $request, Connector $connector)
+    {
+        $year = $request->get("year-choice");
+        $country = $request->get("country-choice");
+        $countryCode = $this->getCountryCode($country, $connector);
+
+        $publicHolidays = $connector->getHolidaysByYearAndCountry($year, $countryCode);
+        $holidayCount = count($publicHolidays);
+        $groupedByMonthHolidays = $connector->groupByMonth($publicHolidays);
+
+        $todayType = $connector->today($countryCode);
+
+        return $this->render('home/holidays.html.twig', [
+            'holidays' => $groupedByMonthHolidays,
+            'todayType' => $todayType,
+            'country' => $country,
+            'year' => $year,
+            'holidayCount' => $holidayCount
+        ]);
+    }
+
+    public function getCountryCode(string $country, Connector $connector) : string
+    {
+        $countryCode = "";
+        $countries = $connector->getSupportedCountries();
+
+        foreach ($countries as $countryItems) {
+            if(array_search($country, $countryItems)) {
+                $countryCode = $countryItems["countryCode"];
+                break;
+            }
+        }
+
+        return $countryCode;
+    }
+
+    public function getMaxNonWorkdaysInARow(array $holidays) {
+        $count = 0;
+        foreach ($holidays as $holiday) {
+
+        }
     }
 }
